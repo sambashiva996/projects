350a42c8d6cfee2d7ee338ff57e96b66fffc4ad1
sambashiva996@gmail.com <sambashiva996@gmail.com>
2025-03-09 13:19:31 +0530

    spring oauth2 client added

---
+++ spring-oauth2-client/src/main/java/com/springoauth2/spring/oauth2/client/SpringOauth2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.springoauth2.spring.oauth2.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ClientApplication.class, args);
+	}
+
+}
---
+++ spring-oauth2-client/src/test/java/com/springoauth2/spring/oauth2/client/SpringOauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springoauth2.spring.oauth2.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
