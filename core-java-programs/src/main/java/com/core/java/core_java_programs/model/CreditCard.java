da162e6c6bf39881c858160bf6c3a0c345b598c9
sambashiva996@gmail.com <sambashiva996@gmail.com>
2025-03-19 20:00:59 +0530

    new project added

---
+++ core-java-programs/src/main/java/com/core/java/core_java_programs/model/CreditCard.java
@@ -0,0 +1,25 @@
+package com.core.java.core_java_programs.model;
+
+import java.util.List;
+
+public class CreditCard {
+	
+	private int id;
+	private String creditCardNumber;
+
+	public static List<CreditCard> getCreditCard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public int getCreditCardNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
