82affd96fe7c8dae28e4203c299b29b0bc90a4c9
sambashiva996@gmail.com <sambashiva996@gmail.com>
2025-03-19 20:04:15 +0530

    changes done

---
+++ core-java-programs/src/main/java/com/core/java/core_java_programs/CoreJavaProgramsApplication.java
@@ -0,0 +1,13 @@
+package com.core.java.core_java_programs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJavaProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJavaProgramsApplication.class, args);
+	}
+
+}
