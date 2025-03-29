f9fab918c2333aca583a5020b90321735f49f7c7
sambashiva996@gmail.com <sambashiva996@gmail.com>
2025-03-29 23:38:39 +0530

    new changes added

--- core-java-programs/src/main/java/com/core/java/programs/questionsandanswers/QuestionsAndAnswers.java
+++ core-java-programs/src/main/java/com/core/java/programs/questionsandanswers/QuestionsAndAnswers.java
@@ -2,7 +2,7 @@ package com.core.java.programs.questionsandanswers;
 
 public class QuestionsAndAnswers {
 
-    /* JAVA
+    /* JAVA :
     * 1. Why serialVersionUID required in Serialization and what the diff btw manually serialVersionUID declared and without manually ?
     * 2. How to create Trippleton class in spring ?
     * 3. Will it work two api URI methods same and except method name in spring boot?
@@ -14,22 +14,47 @@ public class QuestionsAndAnswers {
     * 9. Why wait and notify methods are in Object class ?
     * 10. if the list having student objecting by name is 'abc', then you need to add new student object with id 4 and name 'def'.
           Write a logic using java8 features.
+    * 11. What is constructor calling in java
+    * 12. What special symbols allowed for identifiers in java ?
+    * 13. Java SOLID Principles
+    * 14. How Concurrent HashMap differentiate with HashMap in java ?
     *
     * */
 
    /*
+   * Spring boot :
    * 1. How to handle 1000 multiple request in spring boot code level
    * 2. We have 100 of microservices one of it got down. The root cause is it will take 24H to resolve it.
    *    but we need to provide immediate resolution ? how can achieve it ?
+   * 3. How Maven application become a spring application not spring boot app, and it expects one lib to become spring app ?
+   *    what is it ?
+   * 4. How white label error display when we give wrong url ? what is that default code in spring boot ?
+   * 5. How Transactional annotation works and what are the params in it in spring boot?
+   * 6  What is the response if two api methods have same URI and params except method name in spring boot ?
+   * 7. Spring security with JWT
+   * 8. How Caching works in spring boot and implementation?
+   * 8. How to create triple ton class in spring boot ?
    *
    * */
 
     /*
-    * SQL
-    * 1. Can you please write down an sql query to get an 'n'th highest salary from an Employee table?
+    * SQL :
+    * 1. Can you please write down a sql query to get a 'nth' the highest salary from an Employee table?
     * 2. Write a query for JOINS
     * 3. Write a query for UNION and UNION ALL
     * 4. What are ACID properties and how transaction integrity works
+    * 5. What are the new DDL features ?
     *
      * */
+
+    /*
+    * Microservices :
+    * 1. Microservices high level architecture
+    * 2. What API Gate way and how it works
+    * 3. What Service Discovery server like Eureka server and how it can work and how to integrate with spring boot ?
+    * 4. What are the ways to communicate btw each spring boot microservices ?
+    * 5. What are design patterns and explain each ?
+    * 6. To implement microservices what are the things to do follow ?
+    *
+    * */
 }
