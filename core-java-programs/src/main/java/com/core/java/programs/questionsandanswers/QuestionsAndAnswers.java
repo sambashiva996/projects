4928961f8707e2cd3aebe0fb2d6724c77367a416
sambashiva996@gmail.com <sambashiva996@gmail.com>
2025-03-30 22:36:17 +0530

    new changes added

--- core-java-programs/src/main/java/com/core/java/programs/questionsandanswers/QuestionsAndAnswers.java
+++ core-java-programs/src/main/java/com/core/java/programs/questionsandanswers/QuestionsAndAnswers.java
@@ -2,60 +2,60 @@ package com.core.java.programs.questionsandanswers;
 
 public class QuestionsAndAnswers {
 
-    /* JAVA :
-    * 1. Why serialVersionUID required in Serialization and what the diff btw manually serialVersionUID declared and without manually ?
-    * 2. How to create Trippleton class in spring ?
+    /* JAVA:
+    * 1. Why serialVersionUID required in Serialization and what the diff btw manually serialVersionUID declared and without manually?
+    * 2. How to create Trippleton class in spring?
     * 3. Will it work two api URI methods same and except method name in spring boot?
-    * 4. How to handle millions of records using coding (without any cloud) in spring boot ?
-    * 5. How to handle multiple databases in one spring boot application ?
-    * 6. What is diff btw Integer.parseInt and Integer.valueOf in java ?
-    * 7. How to cover test case for static method which is in injected mock class in current class in spring boot?
+    * 4. How to handle millions of records using coding (without any cloud) in spring boot?
+    * 5. How to handle multiple databases in one spring boot application?
+    * 6. What is diff btw Integer.parseInt and Integer.valueOf in java?
+    * 7. How to cover a test case for static method which is in injected mock class in current class in spring boot?
     * 8. What is diff btw (a==1) & (b==2) and (a==1) && (b==2) ?
-    * 9. Why wait and notify methods are in Object class ?
-    * 10. if the list having student objecting by name is 'abc', then you need to add new student object with id 4 and name 'def'.
+    * 9. Why wait and notify methods are in Object class?
+    * 10. If the list having student objecting by name is 'abc', then you need to add a new student object with id 4 and name 'def'.
           Write a logic using java8 features.
-    * 11. What is constructor calling in java
-    * 12. What special symbols allowed for identifiers in java ?
+    * 11. What is constructor called in java
+    * 12. What special symbols allowed for identifiers in java?
     * 13. Java SOLID Principles
-    * 14. How Concurrent HashMap differentiate with HashMap in java ?
-    * 15. Interface allow static and default methods but still abstract class is there and why ?
+    * 14. How Concurrent HashMap differentiate with HashMap in java?
+    * 15. Interface allows static and default methods, but still abstract class is there, and why?
     *
     * */
 
    /*
-   * Spring boot :
-   * 1. How to handle 1000 multiple request in spring boot code level
-   * 2. We have 100 of microservices one of it got down. The root cause is it will take 24H to resolve it.
-   *    but we need to provide immediate resolution ? how can achieve it ?
-   * 3. How Maven application become a spring application not spring boot app, and it expects one lib to become spring app (i.e : spring-context)?
-   *    what is it ?
-   * 4. How white label error display when we give wrong url ? what is that default code in spring boot (i.e : BasicErrorController)?
-   * 5. How Transactional annotation works and what are the params in it in spring boot?
-   * 6  What is the response if two api methods have same URI and params except method name in spring boot ?
+   * Spring boot:
+   * 1. How to handle 1000 multiple requests in spring boot code level
+   * 2. We have 100 microservices, one of it got down. The root cause is it will take 24H to resolve it.
+   *    But we need to provide immediate resolution ? how can we achieve it?
+   * 3. How does Maven application become a spring application not spring boot app, and it expects one lib to become spring app (i.e : spring-context)?
+   *    What is it?
+   * 4. How white label error display when we give the wrong url ? what is that default code in spring boot (i.e.: BasicErrorController)?
+   * 5. How does Transactional annotation work and what are the params in it in spring boot?
+   * 6. What is the response if two api methods have the same URI and params except method name in spring boot?
    * 7. Spring security with JWT
-   * 8. How Caching works in spring boot and implementation?
-   * 9. How to create triple ton class in spring boot ?
+   * 8. How does Caching work in spring boot and implementation?
+   * 9. How to create triple ton class in spring boot?
    *
    * */
 
     /*
-    * SQL :
-    * 1. Can you please write down a sql query to get a 'nth' the highest salary from an Employee table?
+    * SQL:
+    * 1. Can you please write down an SQL query to get a 'nth' the highest salary from an Employee table?
     * 2. Write a query for JOINS
     * 3. Write a query for UNION and UNION ALL
     * 4. What are ACID properties and how transaction integrity works
-    * 5. What are the new DDL features ?
+    * 5. What are the new DDL features?
     *
      * */
 
     /*
-    * Microservices :
+    * Microservices:
     * 1. Microservices high level architecture
-    * 2. What API Gate way and how it works
-    * 3. What Service Discovery server like Eureka server and how it can work and how to integrate with spring boot ?
-    * 4. What are the ways to communicate btw each spring boot microservices ?
-    * 5. What are design patterns and explain each ?
-    * 6. To implement microservices what are the things to do follow ?
+    * 2. What API gateway and how it works
+    * 3. What Service Discovery server like Eureka server and how it can work and how to integrate with spring boot?
+    * 4. What are the ways to communicate btw each spring boot microservice?
+    * 5. What are design patterns and explain each?
+    * 6. To implement microservices, what are the things to do follow?
     *
     * */
 }
