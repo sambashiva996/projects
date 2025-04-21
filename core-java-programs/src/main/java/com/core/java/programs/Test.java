30fbe0998d632f8648653b6d751606c80d5d913e
sambashiva996@gmail.com <sambashiva996@gmail.com>
2025-04-21 22:06:02 +0530

    new changes added

--- core-java-programs/src/main/java/com/core/java/programs/Test.java
+++ core-java-programs/src/main/java/com/core/java/programs/Test.java
@@ -11,7 +11,16 @@ public class Test {
 
     public static void main(String[] args) {
 
+    /*
+    * input :
+Server 1 Return 3
+Server 2 Return 4
+Server 3 Return 5
+Server 4 Return 4
+Server 5 Return 3
 
+output eg : Return 3 = [Server 1, Return 3]
+    * */
     }
 
 }
